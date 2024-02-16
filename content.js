b7fb3b8cde0a194bc5cbbca88de63b11b69d4451
U.Senthil Kumar <66522620+coderiksenthil@users.noreply.github.com>
2024-02-16 19:56:44 +0530

    Major Update

---
+++ content.js
@@ -0,0 +1,160 @@
+function dynamicClothingSection(ob) {
+  let boxDiv = document.createElement("div");
+  boxDiv.id = "box";
+
+  let boxLink = document.createElement("a");
+  boxLink.href = "/contentDetails.html?" + ob.id;
+
+  let imgTag = document.createElement("img");
+  imgTag.src = ob.preview;
+
+  let detailsDiv = document.createElement("div");
+  detailsDiv.id = "details";
+
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(ob.name);
+  h3.appendChild(h3Text);
+
+  let h4 = document.createElement("h4");
+  let h4Text = document.createTextNode(ob.brand);
+  h4.appendChild(h4Text);
+
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode("Rs " + ob.price);
+  h2.appendChild(h2Text);
+
+  boxDiv.appendChild(boxLink);
+  boxLink.appendChild(imgTag);
+  boxLink.appendChild(detailsDiv);
+  detailsDiv.appendChild(h3);
+  detailsDiv.appendChild(h4);
+  detailsDiv.appendChild(h2);
+
+  return boxDiv;
+}
+
+// Define your data directly here instead of making an AJAX request
+let contentTitle = [
+  {
+      "id": "1",
+      "name": "Product 1",
+      "brand": "Brand 1",
+      "price": "100",
+      "preview": "https://example.com/product1.jpg",
+      "isAccessory": false
+  },
+  {
+      "id": "2",
+      "name": "Product 2",
+      "brand": "Brand 2",
+      "price": "200",
+      "preview": "https://example.com/product2.jpg",
+      "isAccessory": true
+  },
+  // Add more data as needed
+];
+
+let containerClothing = document.getElementById("containerClothing");
+let containerAccessories = document.getElementById("containerAccessories");
+
+// Loop through the data and render it
+for (let i = 0; i < contentTitle.length; i++) {
+  if (contentTitle[i].isAccessory) {
+      containerAccessories.appendChild(dynamicClothingSection(contentTitle[i]));
+  } else {
+      containerClothing.appendChild(dynamicClothingSection(contentTitle[i]));
+  }
+}
+
+
+// console.clear();
+
+/*let contentTitle;
+
+console.log(document.cookie);
+function dynamicClothingSection(ob) {
+  let boxDiv = document.createElement("div");
+  boxDiv.id = "box";
+
+  let boxLink = document.createElement("a");
+  // boxLink.href = '#'
+  boxLink.href = "/contentDetails.html?" + ob.id;
+  // console.log('link=>' + boxLink);
+
+  let imgTag = document.createElement("img");
+  // imgTag.id = 'image1'
+  // imgTag.id = ob.photos
+  imgTag.src = ob.preview;
+
+  let detailsDiv = document.createElement("div");
+  detailsDiv.id = "details";
+
+  let h3 = document.createElement("h3");
+  let h3Text = document.createTextNode(ob.name);
+  h3.appendChild(h3Text);
+
+  let h4 = document.createElement("h4");
+  let h4Text = document.createTextNode(ob.brand);
+  h4.appendChild(h4Text);
+
+  let h2 = document.createElement("h2");
+  let h2Text = document.createTextNode("rs  " + ob.price);
+  h2.appendChild(h2Text);
+
+  boxDiv.appendChild(boxLink);
+  boxLink.appendChild(imgTag);
+  boxLink.appendChild(detailsDiv);
+  detailsDiv.appendChild(h3);
+  detailsDiv.appendChild(h4);
+  detailsDiv.appendChild(h2);
+
+  return boxDiv;
+}
+
+//  TO SHOW THE RENDERED CODE IN CONSOLE
+// console.log(dynamicClothingSection());
+
+// console.log(boxDiv)
+
+let mainContainer = document.getElementById("mainContainer");
+let containerClothing = document.getElementById("containerClothing");
+let containerAccessories = document.getElementById("containerAccessories");
+// mainContainer.appendChild(dynamicClothingSection('hello world!!'))
+
+// BACKEND CALLING
+
+let httpRequest = new XMLHttpRequest();
+
+httpRequest.onreadystatechange = function() {
+  if (this.readyState === 4) {
+    if (this.status == 200) {
+      // console.log('call successful');
+      contentTitle = JSON.parse(this.responseText);
+      if (document.cookie.indexOf(",counter=") >= 0) {
+        var counter = document.cookie.split(",")[1].split("=")[1];
+        document.getElementById("badge").innerHTML = counter;
+      }
+      for (let i = 0; i < contentTitle.length; i++) {
+        if (contentTitle[i].isAccessory) {
+          console.log(contentTitle[i]);
+          containerAccessories.appendChild(
+            dynamicClothingSection(contentTitle[i])
+          );
+        } else {
+          console.log(contentTitle[i]);
+          containerClothing.appendChild(
+            dynamicClothingSection(contentTitle[i])
+          );
+        }
+      }
+    } else {
+      console.log("call failed!");
+    }
+  }
+};
+httpRequest.open(
+  "GET",
+  "https://5d76bf96515d1a0014085cf9.mockapi.io/product",
+  true
+);
+httpRequest.send();*/
